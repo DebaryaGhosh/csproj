6a9441f556a8fc372b648fb09ee627860fb6284a
debaryaghosh <73695474+DebaryaGhosh@users.noreply.github.com>
2023-07-30 11:29:15 +0530

    changes

--- constants.py
+++ constants.py
@@ -2,5 +2,5 @@ WINDOW_SIZE = '400x350'
 WINDOW_TITLE = 'CSPROJ Alpha v0.0.1'
 
 #--------DICTIONARY CONSTANTS----------------------
-WINDOW_ROOT = 'root'
+ROOT = 'root'
 WINDOW_BUTTON_1 = 'button_1'
--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print('hello, world!')
+import tkinter as tk
+import constants as c
+
+widgets = {
+    c.ROOT: None,
+    c.WINDOW_BUTTON_1: None,
+}
+
+def setup_root():
+    global widgets
+
+    widgets[c.ROOT] = tk.Tk()
+    widgets[c.ROOT].title(c.WINDOW_TITLE)
+    widgets[c.ROOT].geometry(c.WINDOW_SIZE)
+    widgets[c.ROOT].resizable(0, 0)
+    
+
+def setup_widgets():
+    global widgets
+
+    widgets[c.WINDOW_BUTTON_1] = tk.Button(
+        widgets[c.ROOT],
+        text='hello world',
+        command=lambda: print('hello')
+    )
+    widgets[c.WINDOW_BUTTON_1].pack()
+
+def main():
+    setup_root()
+    setup_widgets()
+
+main()
+widgets[c.ROOT].mainloop()
