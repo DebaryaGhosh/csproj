128bd52319fb81c1efbc557f8e98140448cc99fe
debaryaghosh <73695474+DebaryaGhosh@users.noreply.github.com>
2023-07-30 12:37:23 +0530

    added buttons

--- constants.py
+++ constants.py
@@ -1,6 +1,16 @@
-WINDOW_SIZE = '400x350'
+WINDOW_SIZE = '900x550'
 WINDOW_TITLE = 'CSPROJ Alpha v0.0.1'
+
+FRAME_WIDTH = 400
+FRAME_HEIGHT = 350
 
 #--------DICTIONARY CONSTANTS----------------------
 ROOT = 'root'
-WINDOW_BUTTON_1 = 'button_1'
+FRAME = 'frame'
+TRAIN_WIN_BTN = 'train win button'
+PLANE_WIN_BTN = 'plane win button'
+
+
+#--------BUTTON STRINGS-------------------------
+TRAIN_WIN_TEXT = 'Trains'
+PLANE_WIN_TEXT = 'Planes'
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 import constants as c
 
 widgets = {
     c.ROOT: None,
-    c.WINDOW_BUTTON_1: None,
+    c.FRAME: None,
+    c.PLANE_WIN_BTN: None,
+    c.TRAIN_WIN_BTN: None,
 }
 
 def setup_root():
@@ -18,16 +21,33 @@ def setup_root():
 def setup_widgets():
     global widgets
 
-    widgets[c.WINDOW_BUTTON_1] = tk.Button(
+    widgets[c.FRAME] = ttk.Frame(
         widgets[c.ROOT],
-        text='hello world',
-        command=lambda: print('hello')
+        width=c.FRAME_WIDTH,
+        height=c.FRAME_HEIGHT,
     )
-    widgets[c.WINDOW_BUTTON_1].pack()
+    widgets[c.FRAME].grid_propagate(0)
+    widgets[c.FRAME].pack()
+
+    widgets[c.TRAIN_WIN_BTN] = ttk.Button(
+        widgets[c.FRAME],
+        text = c.TRAIN_WIN_TEXT,
+        command=lambda: print('open trains window')
+    )
+    widgets[c.TRAIN_WIN_BTN].grid(row=0, column = 1, ipadx = 5, ipady = 3)
+
+    widgets[c.PLANE_WIN_BTN] = ttk.Button(
+        widgets[c.FRAME],
+        text = c.PLANE_WIN_TEXT,
+        command=lambda: print('open plane window')
+    )
+    widgets[c.PLANE_WIN_BTN].grid(row=0, column = 2, ipadx = 5, ipady = 3)
+
 
 def main():
     setup_root()
     setup_widgets()
 
 main()
+print(widgets[c.FRAME])
 widgets[c.ROOT].mainloop()
