cd88c42567e6336c96f2d6bc4b98341a59eec78c
debaryaghosh <73695474+DebaryaGhosh@users.noreply.github.com>
2023-07-30 14:42:56 +0530

    added title, borders

--- constants.py
+++ constants.py
@@ -1,14 +1,19 @@
-WINDOW_SIZE = '900x550'
+WINDOW_SIZE = '825x525'
 WINDOW_TITLE = 'CSPROJ Alpha v0.0.1'
 
-FRAME_WIDTH = 400
-FRAME_HEIGHT = 350
+FRAME_WIDTH = 800
+FRAME_HEIGHT = 500
 
 #--------DICTIONARY CONSTANTS----------------------
 ROOT = 'root'
 FRAME = 'frame'
+TITLE_LBL = 'title label'
+TITLE_LBL_TEXT = 'CSPROJECT'
 TRAIN_WIN_BTN = 'train win button'
 PLANE_WIN_BTN = 'plane win button'
+LOCATIONS_TRVW = 'locations treeview'
+MODE_TRAVEL_LBL = 'mode of travel label'
+MODE_TRAVEL_TEXT =  'Mode Of Travel:'
 
 
 #--------BUTTON STRINGS-------------------------
--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import constants as c
 widgets = {
     c.ROOT: None,
     c.FRAME: None,
+    c.TITLE_LBL: None,
     c.PLANE_WIN_BTN: None,
     c.TRAIN_WIN_BTN: None,
+    c.LOCATIONS_TRVW: None,
+    c.MODE_TRAVEL_LBL: None,
 }
 
 def setup_root():
@@ -21,27 +24,46 @@ def setup_root():
 def setup_widgets():
     global widgets
 
-    widgets[c.FRAME] = ttk.Frame(
+    widgets[c.FRAME] = tk.Frame(
         widgets[c.ROOT],
+        highlightbackground = 'black',
+        highlightthickness = 1,
         width=c.FRAME_WIDTH,
         height=c.FRAME_HEIGHT,
     )
     widgets[c.FRAME].grid_propagate(0)
-    widgets[c.FRAME].pack()
+    widgets[c.FRAME].pack(padx=20, pady=10)
+
+    widgets[c.TITLE_LBL] = ttk.Label(
+        widgets[c.FRAME],
+        text=c.TITLE_LBL_TEXT,
+        font = ("Helvetica", "16", 'bold'),
+    )
+    widgets[c.TITLE_LBL].grid(row=0, column=0, padx=10, pady=5)
+
+    widgets[c.MODE_TRAVEL_LBL] = ttk.Labelframe (
+        widgets[c.FRAME],
+        text=c.MODE_TRAVEL_TEXT,
+    )
+    widgets[c.MODE_TRAVEL_LBL].grid(row=0, column = 1, columnspan=2, padx=100, pady=5)
 
     widgets[c.TRAIN_WIN_BTN] = ttk.Button(
-        widgets[c.FRAME],
+        widgets[c.MODE_TRAVEL_LBL],
         text = c.TRAIN_WIN_TEXT,
         command=lambda: print('open trains window')
     )
-    widgets[c.TRAIN_WIN_BTN].grid(row=0, column = 1, ipadx = 5, ipady = 3)
+    widgets[c.TRAIN_WIN_BTN].grid(row=0, column = 0, padx = 5, pady=3, ipadx = 5, ipady = 3)
 
     widgets[c.PLANE_WIN_BTN] = ttk.Button(
-        widgets[c.FRAME],
+        widgets[c.MODE_TRAVEL_LBL],
         text = c.PLANE_WIN_TEXT,
         command=lambda: print('open plane window')
     )
-    widgets[c.PLANE_WIN_BTN].grid(row=0, column = 2, ipadx = 5, ipady = 3)
+    widgets[c.PLANE_WIN_BTN].grid(row=0, column = 1, padx = 5, pady=3, ipadx = 5, ipady = 3)
+
+    
+
+    
 
 
 def main():
@@ -49,5 +71,4 @@ def main():
     setup_widgets()
 
 main()
-print(widgets[c.FRAME])
 widgets[c.ROOT].mainloop()
